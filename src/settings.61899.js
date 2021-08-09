3783abf043bb4c075927daa249669c8434aa59b4
PandaTNA <anhtinhbkit@gmail.com>
2021-08-09 08:40:48 +0700

    remove unused module

--- src/settings.61899.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/game.fire",orientation:"",jsList:[],bundleVers:{internal:"69b9a",resources:"261c6",main:"fb40b"}};
